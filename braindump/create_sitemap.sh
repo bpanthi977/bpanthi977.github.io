1dd3e4600ae552532590c037d2a21e0fa5877ae6
Bibek Panthi <bpanthi977@gmail.com>
2025-01-17 11:36:05 -0600

    Update sitemap

--- braindump/create_sitemap.sh
+++ braindump/create_sitemap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find . -name "*.html" -not -path "*/data/*"  | sed "s/.\\//https:\\/\\/bpanthi977.github.io\\/braindump\\//" > sitemap.txt
+find . -name "*.html" -not -path "*/data/*"  | sed "s/.\\//https:\\/\\/bpanthi977.com\\/braindump\\//" > sitemap.txt
 
 ./create_sitemap.lisp
